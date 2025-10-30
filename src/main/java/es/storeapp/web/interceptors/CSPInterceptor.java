0e064c14e14edef822326fdd3b3dd20d6d3cff3d
dopa <diego.dopazo.gar@gmail.com>
2025-10-30 15:57:19 +0100

    feat: improve content security policy

--- src/main/java/es/storeapp/web/interceptors/CSPInterceptor.java
+++ src/main/java/es/storeapp/web/interceptors/CSPInterceptor.java
@@ -5,16 +5,21 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 public class CSPInterceptor implements HandlerInterceptor {
-    
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
         response.setHeader("Content-Security-Policy",
-                "default-src * 'self'; " +
-                        "img-src * 'self' data:; " +
-                        "script-src  * 'self' 'unsafe-eval' 'unsafe-inline'; " +
-                        "style-src   * 'self' 'unsafe-inline';");
+                "default-src 'self'; " +                            //solo recursos de mi origen
+                        "img-src 'self' data:; " +                              //solo imagenes de mi dominio
+                        "script-src 'self' 'unsafe-inline'; " +                 //solo js de mi app
+                        "style-src 'self' 'unsafe-inline';"+                    //solo css de mi dominio
+                        "object-src 'none';"+                                   //prohibe el uso de flash, java applets, etc
+                        "base-uri 'self'; "+                                    //prohibe que cambien la uri
+                        "connect-src 'self'; "+                                 //prohibe conexiones AJAX fuera de mi dominio
+                        "frame-ancestors 'none';"+                              //prohibe clickjacking xq no deja embeber en iframe
+                        "form-action 'self';");                                 //solo envia forms a mi dominio
         return true;
     }
-    
+
 }
