712f014334a93bf27f4f742b4f1b0937611e6df7
SergioLuaces <91591778+SergioLuaces@users.noreply.github.com>
2025-11-06 19:06:00 +0100

    Add method to find Order by id and userId

--- src/main/java/es/storeapp/business/repositories/OrderRepository.java
+++ src/main/java/es/storeapp/business/repositories/OrderRepository.java
@@ -3,6 +3,8 @@ package es.storeapp.business.repositories;
 import es.storeapp.business.entities.Order;
 import java.text.MessageFormat;
 import java.util.List;
+import java.util.Optional;
+
 import jakarta.persistence.Query;
 import org.springframework.stereotype.Repository;
 
@@ -15,6 +17,18 @@ public class OrderRepository extends AbstractRepository<Order> {
     public List<Order> findByUserId(Long userId) {
         Query query = entityManager.createQuery(MessageFormat.format(FIND_BY_USER_QUERY, userId));
         return (List<Order>) query.getResultList();
+    }
+
+    public Optional<Order> findByIdAndUserId(Long id, Long userId) {
+        return entityManager.createQuery(
+                        "SELECT o FROM Order o WHERE o.orderId = :id AND o.user.userId = :userId",
+                        Order.class
+                )
+                .setParameter("id", id)
+                .setParameter("userId", userId)
+                .getResultList()
+                .stream()
+                .findFirst();
     }
    
 }
