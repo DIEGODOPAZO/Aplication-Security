76ab345ec1a5d473f3155310310c6ca557468aa9
dopa <diego.dopazo.gar@gmail.com>
2025-11-07 12:59:26 +0100

    arreglado error al pagar con targeta por defecto

--- src/main/java/es/storeapp/web/forms/PaymentForm.java
+++ src/main/java/es/storeapp/web/forms/PaymentForm.java
@@ -9,27 +9,12 @@ import jakarta.validation.constraints.Size;
 
 public class PaymentForm {
 
+
     private Boolean defaultCreditCard;
-
-    @NotBlank(message = "El número de tarjeta es obligatorio")
-    @Pattern(regexp = "\\d{13,19}", message = "El número de tarjeta debe tener entre 13 y 19 dígitos")
     private String creditCard;
-
-    @NotNull(message = "El CVV es obligatorio")
-    @Min(value = 100, message = "El CVV debe tener 3 dígitos")
-    @Max(value = 9999, message = "El CVV no puede tener más de 4 dígitos")
     private Integer cvv;
-
-    @NotNull(message = "El mes de expiración es obligatorio")
-    @Min(value = 1)
-    @Max(value = 12)
     private Integer expirationMonth;
-
-    @NotNull(message = "El año de expiración es obligatorio")
-    @Min(value = 2024)
-    @Max(value = 2100)
     private Integer expirationYear;
-
     private Boolean save;
 
     public Boolean getDefaultCreditCard() {
@@ -39,7 +24,7 @@ public class PaymentForm {
     public void setDefaultCreditCard(Boolean defaultCreditCard) {
         this.defaultCreditCard = defaultCreditCard;
     }
-    
+
     public String getCreditCard() {
         return creditCard;
     }
